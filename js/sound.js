bae32228cda825311746fa1a46f74b1a4ce85f3c
Yuanze Zhang <jason950314@gmail.com>
2017-11-29 00:07:02 -0500

    added loop function

--- js/sound.js
+++ js/sound.js
@@ -1,21 +1,83 @@
+var $currentloop = $('#D');
+
+function playing(elem){
+    $(elem).css({
+            backgroundColor: '#e1e2e4'
+        });
+    $(elem).animate({
+        backgroundColor: 'none'
+    }, 4000);
+}
+
+function checkLoop(elem){
+    if ($('#loop').prop('checked') == false){
+        $currentloop.stop();
+        $currentloop.removeAttr("data-loop");
+    }
+    if ($(elem).attr('data-loop')){ 
+        console.log('hi');
+        $(elem)[0].currentTime = 0;
+        $(elem)[0].play();
+        playing(elem);
+    }
+}
+
+function setLoop(elem){
+    $currentloop.stop();
+    $currentloop.removeAttr("data-loop");
+    $currentloop = $(elem);
+    $(elem).attr('data-loop','1');
+}
+
 $(document).ready(function(){
     $('#playD').click(function(){
-        $('#D').get(0).play();
-        console.log("play");
+        if ($('#loop').prop('checked')){
+            setLoop($('#D'));
+        }
+        $('#D')[0].currentTime = 0;
+        $('#D')[0].play();
+        playing($(this));
     });
+
     $('#playA').click(function(){
-        $('#A').get(0).play();
+        if ($('#loop').prop('checked')){
+            setLoop($('#A'));
+        }
+        $('#A')[0].currentTime = 0;
+        $('#A')[0].play();
+        playing($(this));
     });
     $('#playE2').click(function(){
-        $('#E2').get(0).play();
+        if ($('#loop').prop('checked')){
+            setLoop($('#E2'));
+        }
+        $('#E2')[0].currentTime = 0;
+        $('#E2')[0].play();
+        playing($(this));
     });
     $('#playG').click(function(){
-        $('#G').get(0).play();
+        if ($('#loop').prop('checked')){
+            setLoop($('#G'));
+        }
+        $('#G')[0].currentTime = 0;
+        $('#G')[0].play();
+        playing($(this));
     });
     $('#playB').click(function(){
-        $('#B').get(0).play();
+        if ($('#loop').prop('checked')){
+            setLoop($('#B'));
+        }
+        $('#B')[0].currentTime = 0;
+        $('#B')[0].play();
+        playing($(this));
     });
     $('#playE4').click(function(){
-        $('#E4').get(0).play();
+        if ($('#loop').prop('checked')){
+            setLoop($('#E4'));
+        }
+        $('#E4')[0].currentTime = 0;
+        $('#E4')[0].play();
+        playing($(this));
     });
-})
+});
+
