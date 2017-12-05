ffaf761cecb716f1ba42e593ff200370e1d6435d
Yuanze Zhang <jason950314@gmail.com>
2017-12-05 00:43:36 -0500

    finished mobile nav

--- js/instruments.js
+++ js/instruments.js
@@ -16,18 +16,18 @@ console.log("here");
 })
 
 
-         function playSound(el,soundfile) {
-              var embed = document.getElementById("embed");
-              if (!embed) {
-                  var embed = document.createElement("embed");
-                      embed.id= "embed";
-                      embed.setAttribute("src", soundfile);
-                      embed.setAttribute("hidden", "true");
-                  el.appendChild(embed);
-              } else {
-                  embed.parentNode.removeChild(embed);
-              }
-          }
+function playSound(el,soundfile) {
+    var embed = document.getElementById("embed");
+    if (!embed) {
+        var embed = document.createElement("embed");
+            embed.id= "embed";
+            embed.setAttribute("src", soundfile);
+            embed.setAttribute("hidden", "true");
+        el.appendChild(embed);
+    } else {
+        embed.parentNode.removeChild(embed);
+    }
+}
 
 class StickyNavigation {
   
